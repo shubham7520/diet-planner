9286510658efdbe78319895cc65ca46525172f3f
Shubham Singh <shubhambaba143143@gmail.com>
2022-07-26 01:53:09 +0530

    evo_3

--- routes/meal.js
+++ routes/meal.js
@@ -1,5 +1,7 @@
 const mealController = require("../controllers/mealController");
+const meal = require("../models/meal");
 
 module.exports = (app) => {
   app.post("/add-meal", mealController.addMeal);
+  app.put("/update-meal", mealController.updateMeal);
 };
